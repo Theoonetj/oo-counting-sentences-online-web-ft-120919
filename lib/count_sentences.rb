e44a6b8e40ed3ad7ff477a6a6bb233d697246941
confident-token-1019 <theoonetj@gmail.com>
2019-12-22 05:45:15 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,23 +3,31 @@ require 'pry'
 class String
 
   def sentence?
-    self.end_with?(".")
+    if self.end_with?(".")
+      return true
+    else
+      return false
+    end
   end
 
   def question?
-    if self.end_with?(" ?")
-      return true 
-    else 
-      return false 
+    if self.end_with?("?")
+      return true
+    else
+      return false
+    end
   end
 
   def exclamation?
-     self.end_with?("!")
+    if self.end_with?("!")
+      return true
+    else
+      return false
+    end
   end
 
   def count_sentences
-   self.split(/[.!?]/).reject {|x| x.empty?}.size 
+    new_array = self.split(/[.?!]/).reject {|string| string.empty?}
+    return new_array.length
   end
 end
-
-
